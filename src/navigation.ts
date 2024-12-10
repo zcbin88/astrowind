4e1d15a419201a0be80849c1fd1a8ee2c74c773d
Bingo <zcbin2@grgbanking.com>
2024-12-10 15:18:16 +0800

    init chainfind

--- src/navigation.ts
+++ src/navigation.ts
@@ -3,7 +3,11 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
-      text: 'Homes',
+      text: 'Home',
+      href: getPermalink('')
+    },
+    {
+      text: 'Navigation',
       links: [
         {
           text: 'SaaS',
@@ -115,7 +119,11 @@ export const headerData = {
       href: '#',
     },
   ],
-  actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
+  actions: [
+  
+  { text: 'Login', href: '#', target: '_blank' }
+  
+  ],
 };
 
 export const footerData = {
@@ -178,6 +186,6 @@ export const footerData = {
   ],
   footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    · All rights reserved. <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> Chainfind</a>
   `,
 };
